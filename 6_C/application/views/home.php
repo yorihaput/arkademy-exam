3f43749e9a23f7f7948616a698e45968152b5661
yorihaput <yori.haput@gmail.com>
2019-10-27 00:31:11 +0700

    Percobaan Upload 16

--- 6_C/application/views/home.php
+++ 6_C/application/views/home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Soal 6.B</title>
+    <title>Soal 6.C</title>
 
     <!-- CSS FILE -->
     <link rel="stylesheet" type="text/css" href="./assets/bootstrap/css/bootstrap.min.css">
